74b70b35d002c6f8b0d24e7d265f5eef47ac08eb
Ikmal <fajrikmal141@gmail.com>
2026-06-08 22:47:43 +0700

    fix: improve guest import validation

--- app/Imports/GuestImport.php
+++ app/Imports/GuestImport.php
@@ -4,11 +4,14 @@ namespace App\Imports;
 
 use App\Models\Guest;
 use Maatwebsite\Excel\Concerns\SkipsEmptyRows;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithCustomCsvSettings;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithValidation;
+use Maatwebsite\Excel\Validators\Failure;
 
-class GuestImport implements ToModel, WithHeadingRow, WithValidation, SkipsEmptyRows
+class GuestImport implements ToModel, WithHeadingRow, WithValidation, SkipsEmptyRows, SkipsOnFailure, WithCustomCsvSettings
 {
     /**
     * @param array $row
@@ -17,6 +20,7 @@ class GuestImport implements ToModel, WithHeadingRow, WithValidation, SkipsEmpty
     */
 
     protected string $eventId;
+    protected array $failures = [];
 
     public function __construct(string $eventId)
     {
@@ -27,7 +31,9 @@ class GuestImport implements ToModel, WithHeadingRow, WithValidation, SkipsEmpty
     {
         return new Guest([
             'event_id' => $this->eventId,
+            'nomor_undangan' => $row['nomor_undangan'],
             'nama_utama' => $row['nama_utama'],
+            'jumlah_tamu' => !empty($row['jumlah_tamu']) ? (int) $row['jumlah_tamu'] : 1,
         ]);
     }
 
@@ -35,6 +41,33 @@ class GuestImport implements ToModel, WithHeadingRow, WithValidation, SkipsEmpty
     {
         return [
             'nama_utama' => 'required|string',
+            'jumlah_tamu' => 'nullable|integer|min:1',
+        ];
+    }
+
+    public function customValidationMessages(): array
+    {
+        return [
+            'nama_utama.required' => 'Kolom nama_utama tidak boleh kosong.',
+            'jumlah_tamu.integer' => 'Kolom jumlah_tamu harus berupa angka.',
+            'jumlah_tamu.min' => 'Kolom jumlah_tamu minimal 1.',
+        ];
+    }
+
+    public function onFailure(Failure ...$failure): void
+    {
+        $this->failures = array_merge($this->failures, $failure);
+    }
+
+    public function getFailures(): array
+    {
+        return $this->failures;
+    }
+
+    public function getCsvSettings(): array
+    {
+        return [
+            'delimiter' => ';',
         ];
     }
 }
